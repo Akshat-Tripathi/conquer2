0a3998271bd79eee30cecdab492defe91f2f5662
Shashwat Kansal <shashwatkansal@shashwats-air.home>
2020-04-08 04:10:15 +0100

    React template initiated

---
+++ frontend/conquer2/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>UNDER DEVELOPMENT</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By Shashwat Kansal and Akshat Tripathi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
