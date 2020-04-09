d37b2c51fbd6d433b024666c83438463bf250217
Akshat Tripathi <at619@ic.ac.uk>
2020-04-09 21:53:26 +0100

    communications written

--- backend/games/connectionManager.go
+++ backend/games/connectionManager.go
@@ -1,6 +1,52 @@
 package game
 
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
+)
+
 type connectionManager struct {
+	players map[string]chan Action //TODO - make specific outgoing msg structs if errors
+	msgs    chan Action
 }
 
-func (c *connectionManager) addConnection()
+//Monitor - monitors a player's websocket
+func (c *connectionManager) Monitor(name string, conn *websocket.Conn) {
+	outboundMsgs := make(chan Action)
+	c.players[name] = outboundMsgs
+	var act Action
+	for {
+		err := conn.ReadJSON(&act)
+		if err != nil {
+			log.Fatal("User left")
+			delete(c.players, name)
+			return
+		}
+		act.Player = name
+		c.msgs <- act
+		select {
+		case msg := <-outboundMsgs:
+			err = conn.WriteJSON(msg)
+			if err != nil {
+				log.Fatal("User left")
+				delete(c.players, name)
+				return
+			}
+		}
+	}
+}
+
+func (c *connectionManager) sendToAll(msg Action) {
+	for _, v := range c.players {
+		v <- msg
+	}
+}
+
+func (c *connectionManager) sendToPlayer(msg Action, player string) {
+	c.players[player] <- msg
+}
--- backend/games/normalGame.go
+++ backend/games/normalGame.go
@@ -1,7 +1,33 @@
 package game
 
+import (
+	"net/http"
+	"sync/atomic"
+)
+
 //NormalGame - basic test version
 type NormalGame struct {
-	state map[string]gameState
-	conn  connectionManager
+	id          string
+	state       map[string]gameState
+	conn        connectionManager
+	numPlayers  int32
+	playerLimit int32
+}
+
+//Handle - a handles all new players
+func (ng *NormalGame) Handle(w http.ResponseWriter, r *http.Request) {
+	if ng.numPlayers < ng.playerLimit {
+		atomic.AddInt32(&ng.numPlayers, 1)
+		http.Redirect(w, r, "/gamefull", http.StatusFound)
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		panic(err)
+	}
+	cookie, err := r.Cookie("username")
+	if err != nil {
+		panic(err)
+	}
+	name := cookie.Value
+	ng.conn.Monitor(name, conn)
 }
