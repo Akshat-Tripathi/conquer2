8e5a481664f7e0c28ff9a1c9e9b39fdd8d5b64d0
Akshat Tripathi <at619@ic.ac.uk>
2020-05-17 11:54:21 +0100

    set up project properly

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 var (
 	neighbours = loadMap()
 )
 
+type Home struct {
+	Title string
+}
+
 func main() {
 	fmt.Println(neighbours["A"])
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+
+	r.Use(static.Serve("/static", static.LocalFile("./frontend/home", true)))
+
+	r.LoadHTMLGlob("frontend/home/*.html")
+	r.GET("/abc", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", Home{"Akshat"})
+	})
+	r.Run(":" + port)
 }
