dfea877671b82ca9a9d4dd0791382e4ea1831c22
Shashwat Kansal <shashwatkansal@shashwats-air.home>
2020-05-18 15:14:29 +0100

    yarn build

---
+++ frontend/build/precache-manifest.38667043d49d3da1445e23871612820e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215c2d61cc3c2a8e8e80956330e835e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c1510651a8ac6c735c4",
+    "url": "/static/css/main.014f426f.chunk.css"
+  },
+  {
+    "revision": "ec659910dcdf782fa064",
+    "url": "/static/js/2.71a3349e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.71a3349e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1510651a8ac6c735c4",
+    "url": "/static/js/main.69297f5a.chunk.js"
+  },
+  {
+    "revision": "8cd4f7eed563101f7ede",
+    "url": "/static/js/runtime-main.689c1d4d.js"
+  },
+  {
+    "revision": "ebe2f0405fcbf4a71918d265f066bf53",
+    "url": "/static/media/HomeBackgroundVideo.ebe2f040.mp4"
+  }
+]);
