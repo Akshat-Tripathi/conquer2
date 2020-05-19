ee87a4d11b58436ce25e46e730ab21ceeb3e9f48
Shashwat Kansal <shashwatkansal@shashwats-air.home>
2020-05-19 22:27:23 +0100

    Added loader for html rendering as react components

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,11 +1,30 @@
-// import React, { Component } from "react";
-// //svgWorldMap
-// const MapPolicy = require("frontend/src/pages/SVG-World-Map/demo/basics.html");
+import React, { Component } from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
-// class MapDisplay extends Component {
-//   render() {
-//     return <WebView source={MapPolicy} style={{ flex: 1 }} />;
-//   }
-// }
+//svgWorldMap
 
-// export default MapDisplay;
+// const worldStates = require("frontend/src/pages/SVG-World-Map/src/world-states.svg");
+// const worldStatesProvinces = require("frontend/src/pages/SVG-World-Map/src/world-states-provinces.svg");
+
+const geoURL =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+class MapDisplay extends Component {
+  render() {
+    return (
+      <div>
+        <ComposableMap>
+          <Geographies geography={geoURL}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
+    );
+  }
+}
+
+export default MapDisplay;
