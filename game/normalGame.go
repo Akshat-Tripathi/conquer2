34cba52ae4587381f4d5c7763c0d7be06975b487
Akshat Tripathi <at619@ic.ac.uk>
2020-05-20 20:00:18 +0100

    starting ws test

--- game/normalGame.go
+++ game/normalGame.go
@@ -45,6 +45,7 @@ func (rtg *RealTimeGame) Start(ctx Context, neighbours map[string][]string) {
 	go rtg.processActions()
 	go rtg.processTroops()
 
+	//TODO abstract the router out of this struct
 	rtg.Router.GET("/game/"+ctx.ID+"/ws", func(c *gin.Context) {
 		rtg.handleGame(c.Writer, c.Request)
 	})
