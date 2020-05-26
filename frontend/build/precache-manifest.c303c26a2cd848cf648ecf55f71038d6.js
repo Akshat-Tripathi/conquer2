35f3777e7057457085d01e2f2137ebe0dca47557
Akshat Tripathi <at619@ic.ac.uk>
2020-05-26 17:48:27 +0100

    Added websocket handling on the frontend

---
+++ frontend/build/precache-manifest.c303c26a2cd848cf648ecf55f71038d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989647f672a95613c0b900c5be1bc20d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24798d423b9786a3db6c",
+    "url": "/static/css/main.8ecbb9f5.chunk.css"
+  },
+  {
+    "revision": "b07fb8b3b99ef326e954",
+    "url": "/static/js/2.8f407a7b.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.8f407a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24798d423b9786a3db6c",
+    "url": "/static/js/main.5e2d9467.chunk.js"
+  },
+  {
+    "revision": "1faae516b427c701ae01",
+    "url": "/static/js/runtime-main.89a75888.js"
+  },
+  {
+    "revision": "ebe2f0405fcbf4a71918d265f066bf53",
+    "url": "/static/media/HomeBackgroundVideo.ebe2f040.mp4"
+  },
+  {
+    "revision": "7e83269dc0c013c1636335c6250497c2",
+    "url": "/static/media/intro.7e83269d.mp3"
+  },
+  {
+    "revision": "0020f13a175ce6df3718f65d1b830872",
+    "url": "/static/media/world.0020f13a.txt"
+  }
+]);
