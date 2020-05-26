80c9cbc1ceb12104525f1804673ed4d2630c5aaf
Shashwat Kansal <shashwatkansal@shashwats-air.home>
2020-05-26 15:41:38 +0100

    clearing changes for pull

--- frontend/src/pages/rough.js
+++ frontend/src/pages/rough.js
@@ -1,21 +1,19 @@
-import fetch from "react";
+function getCountryCodes(countrycode) {
+  var fs = require("fs");
+  //enter country to search
+  var textByLine = fs.readFileSync("../maps/world.txt").toString().split("\n");
+  var countriesBordering = [];
 
-// function getCountryCodes(countrycode) {
-//   var fs = require("fs");
-//   //enter country to search
-//   var textByLine = fs.readFileSync("../maps/world.txt").toString().split("\n");
-//   var countriesBordering = [];
+  for (let j = 0; j < textByLine.length; j++) {
+    var borders = textByLine[j].split(" ");
+    if (borders[0] == countrycode) {
+      for (let i = 1; i < borders.length; i++) {
+        //Get border codes
+        countriesBordering.push(borders[i]);
+      }
+    }
+  }
+  return countriesBordering;
+}
 
-//   for (let j = 0; j < textByLine.length; j++) {
-//     var borders = textByLine[j].split(" ");
-//     if (borders[0] == countrycode) {
-//       for (let i = 1; i < borders.length; i++) {
-//         //Get border codes
-//         countriesBordering.push(borders[i]);
-//       }
-//     }
-//   }
-//   return countriesBordering;
-// }
-
-// console.log(getCountryCodes("UK").includes("FR"));
+console.log(getCountryCodes("UK").includes("FR"));
