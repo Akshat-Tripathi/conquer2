cd30dd7a1a79b6c5fdc1eb94da8ae38d17a4dbf9
Shashwat Kansal <shashwatkansal@shashwats-air.home>
2020-06-23 17:37:16 +0100

    fixed loading colours

---
+++ frontend/build/precache-manifest.36d915ca847c5e7fbf3f9c4805dd1066.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23dee6433ad207029dd65c767dd7be1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25712e39bf60065e076a",
+    "url": "/static/css/main.cce9028f.chunk.css"
+  },
+  {
+    "revision": "7e05e1ab6ab32781d975",
+    "url": "/static/js/2.e1dcd6d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e1dcd6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25712e39bf60065e076a",
+    "url": "/static/js/main.de7447e1.chunk.js"
+  },
+  {
+    "revision": "8cd4f7eed563101f7ede",
+    "url": "/static/js/runtime-main.689c1d4d.js"
+  },
+  {
+    "revision": "ebe2f0405fcbf4a71918d265f066bf53",
+    "url": "/static/media/HomeBackgroundVideo.ebe2f040.mp4"
+  },
+  {
+    "revision": "7e83269dc0c013c1636335c6250497c2",
+    "url": "/static/media/intro.7e83269d.mp3"
+  }
+]);
