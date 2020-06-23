3ee383f738d5dd63da65531faf00939cb618c2e9
Shashwat Kansal <shashwatkansal@shashwats-air.home>
2020-06-23 18:18:57 +0100

    Disabled second click unless it's a neighbouring land

---
+++ frontend/build/precache-manifest.f61435154827221936b31315205f8c90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087b3e06f9dc809b1338a3ca0a3076c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f211c955da828be6a79",
+    "url": "/static/css/main.cce9028f.chunk.css"
+  },
+  {
+    "revision": "7e05e1ab6ab32781d975",
+    "url": "/static/js/2.e1dcd6d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e1dcd6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f211c955da828be6a79",
+    "url": "/static/js/main.7964b3ac.chunk.js"
+  },
+  {
+    "revision": "8cd4f7eed563101f7ede",
+    "url": "/static/js/runtime-main.689c1d4d.js"
+  },
+  {
+    "revision": "ebe2f0405fcbf4a71918d265f066bf53",
+    "url": "/static/media/HomeBackgroundVideo.ebe2f040.mp4"
+  },
+  {
+    "revision": "7e83269dc0c013c1636335c6250497c2",
+    "url": "/static/media/intro.7e83269d.mp3"
+  }
+]);
