db406125cbe8c843e7ae3657a6d629ef51987d39
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2020-08-31 18:06:55 +0100

    heroku no longer builds a sourcemap

--- push_to_heroku.py
+++ push_to_heroku.py
@@ -20,11 +20,23 @@ def comment(set):
     with open(".gitignore", 'w') as file:
         file.write(raw)
 
+def toggle_sourcemap():
+    sourcemap = "\nGENERATE_SOURCEMAP=false"
+    with open(".env", "w") as file:
+        txt = file.read()
+        if sourcemap in txt:
+            txt.replace(sourcemap, "")
+        else:
+            txt += sourcemap
+        file.write(txt)
+
 comment(True)
+toggle_sourcemap()
 system("git add .")
 system("git commit -m \"heroku release\"")
 system("git push heroku master --force")
 system("git reset --soft HEAD~1")
 system("git rm -r --cached internal/game/*.json")
+toggle_sourcemap()
 comment(False)
 system("git add .gitignore")
