181d2039b77658a4c4106c23c4593c508ce015b8
Shashwat <shashwatkansal@google.com>
2021-07-24 12:35:48 +0000

    Improve play page with footer header and formatting

--- src/pages/Website/Pages/Play.jsx
+++ src/pages/Website/Pages/Play.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import './Play.css';
-import Logo from '../../../media/conquer2logo.png';
-import StartGameBox from '../StartGameBox';
-import Preloader from '../ComingSoon/Preloader/Preloader';
+import React from "react";
+import "./Play.css";
+import Logo from "../../../media/conquer2logo.png";
+import StartGameBox from "../StartGameBox";
+import Preloader from "../ComingSoon/Preloader/Preloader";
+import Header from "../Header";
+import Footer from "../Footer";
+import Fireball from "../../../media/fireball.mp4"
+import "../Jumbotron.css"
 
 function Play() {
-	return (
-		<div className="play-wrapper">
-			<header>
-				<img src={Logo} />
-				<h6 style={{ color: 'white' }}>By Imperium Games &copy;</h6>
-			</header>
-			<div className="gamebox">
-				<StartGameBox />
-				{/* Sign In here */}
-			</div>
-			<Preloader/>
-		</div>
-	);
+  return (
+    <div className="">
+      <Header />
+      <div className="hero-container">
+        <video src={Fireball} autoPlay loop muted />
+        <div className="grid grid-cols-3 grid-gap-2">
+          <div className="col-span-1 col-start-2 p-16">
+            <StartGameBox />
+            {/* Sign In here */}
+          </div>
+          <Preloader />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Play;
